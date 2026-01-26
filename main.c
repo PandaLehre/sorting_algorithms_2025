7942cd89fdc4c046cf5993283b9b12c755c62f6b
Noah9935 <noah.boufercha@gmx.de>
2026-01-26 13:01:31 +0100

    Can Take 1 Input from an txt file and sort it with Bogo

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 
 #include "sort1_bogo.h"
 
-const int MAX_LINES = 10;
-const int MAX_LINE_LEN = 256;
+const size_t MAX_LINES = 10;
+const size_t MAX_LINE_LEN = 256;
 
 int main(){
     //Sets Seed for randomizer based on current Time
-    srand(time(NULL));
+    srand((unsigned)time(NULL));
 
-
+    /*
     //Bogo Test
     int testArray[] = {7, 3, 2, 4, 1};
     size_t n = sizeof(testArray) / sizeof(testArray[0]);
@@ -22,7 +22,7 @@ int main(){
         printf("%d ", testArray[i]);
     }
     printf("\n");
-
+    */
 
     //Reads File and gives char array back
     FILE *file = fopen("tests/small_input.txt", "r");
@@ -30,18 +30,48 @@ int main(){
         printf("Datei konnte nicht geoeffnet werden.\n");
         return 1;
     }
-    char linesOutput[MAX_LINES][MAX_LINE_LEN];
-    size_t lineCount = 0;
 
-    while ((int)lineCount < MAX_LINES && fgets(linesOutput[lineCount], MAX_LINE_LEN, file)) {
-        lineCount++;
+    int numbersPerLine[MAX_LINES][MAX_LINE_LEN];
+    size_t valuesPerLine[MAX_LINES];
+    size_t numLines = 0;
+
+    while (numLines < MAX_LINES) {
+        size_t numValues = 0;
+
+        while (numValues < MAX_LINE_LEN && fscanf(file, "%d", &numbersPerLine[numLines][numValues]) == 1) 
+        {
+            numValues++;
+        }
+
+        if (numValues == 0) break;
+
+        valuesPerLine[numLines] = numValues;
+        numLines++;
+
+        fscanf(file, "%*[^\n]\n");
     }
     fclose(file);
 
-    //Test Output
-    for (size_t i = 0; i < lineCount; i++) {
-        printf("Zeile %lld: %s", i+1, linesOutput[i]);
+
+    for (size_t i = 0; i < numLines; i++)
+    {
+        bogosort(numbersPerLine[i], valuesPerLine[i]);
     }
+    
+
+
+    //Test Output
+    for (size_t i = 0; i < numLines; i++) {
+        printf("Zeile %zu: ", i + 1);
+        for (size_t j = 0; j < valuesPerLine[i]; j++) {
+            printf("%d ", numbersPerLine[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+
 
     return 0;
 }
